a969a45e5acbb190dee7bd7897d2dd0d80819c87
LAB LAB <louisbrechon@louiss-macbook-air.home>
2023-01-24 22:02:33 +0100

    Ajouts des fichiers

--- FrontEnd/assets/login.js
+++ FrontEnd/assets/login.js
@@ -1,29 +1,35 @@
-$(document).ready(function() {  //https://learn.jquery.com/using-jquery-core/document-ready/
-    const form = document.getElementById('login-form');
-    form.addEventListener('submit', async (event) => {
-        event.preventDefault();
-        const endpoint = 'http://localhost:5678/api/users/login';
-        const email = document.getElementById('email').value;
-        const password = document.getElementById('password').value;
+document.addEventListener("DOMContentLoaded", function(event) {
 
-        const data = { email, password };
+	const form = document.getElementById('login-form');
+	form.addEventListener('submit', function(event) {
+		event.preventDefault();
+		const email = document.getElementById('email').value;
+		const password = document.getElementById('password').value;
+		const data = { email, password };
 
-        const response = await fetch(endpoint, { //https://dmitripavlutin.com/javascript-fetch-async-await/
-        method: 'POST', //send data to server, body type of request is specified by Content-type
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(data) //convert a JS object into a string
-        });
+		//https://dmitripavlutin.com/javascript-fetch-async-await/
+		fetch('http://localhost:5678/api/users/login', {
+			method: 'POST',																					// Send data to server, body type of request is specified by Content-type
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify(data)															// Convert a JS object into a string
+		})
+		.then(function(response) {
+			return response.json();
+		})
+		.then(function(json) {
+			if(json.message == "user not found") {
+				alert("Erreur dans l’identifiant ou le mot de passe");
+			} else {
+				sessionStorage.setItem("userId", json.userId);				// https://developer.mozilla.org/fr/docs/Web/API/Window/sessionStorage
+				sessionStorage.setItem("token", json.token);					// Limited = cleared when the page session ends.
+				location.href = "index.html";
+			}
+		})
+		.catch((err) => {
+			console.log(err);
+		});
 
-        const json = await response.json();
-
-        if (json.error) {
-            alert("Erreur dans l’identifiant ou le mot de passe");
-        } else {
-            sessionStorage.setItem("userId", json.userId); //https://developer.mozilla.org/fr/docs/Web/API/Window/sessionStorage
-            sessionStorage.setItem("token", json.token);    //limited = cleared when the page session ends.
-            location.href="index.html";
-        }
-    });
+	});
 });
 
 //JSON JS Object Notation 
