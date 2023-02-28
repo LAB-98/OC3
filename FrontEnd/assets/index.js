4390e39d6ca32bc90cf30aed55806de1d694f2a9
LAB LAB <you@example.com>
2023-02-28 19:03:01 +0100

    Ajout de fichiers

--- FrontEnd/assets/index.js
+++ FrontEnd/assets/index.js
@@ -87,7 +87,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 	// Handling modal opening
 	document.getElementById('modal-open').addEventListener('click', function(event) {
-		var modal = document.getElementById("myModal");
+		let modal = document.getElementById("myModal");
 		modal.style.display = "block";
 		// Getting existing works
 
@@ -95,57 +95,119 @@ document.addEventListener("DOMContentLoaded", function(event) {
 		// On refait le même fetch qu'en haut
 		// On vide la liste des précédentes images de la popin
 		// On créer les <img> et les boutons : éditer, delete, move
-  fetch("http://localhost:5678/api/works")
-  .then(response => response.json())
-  .then(data => {
-    // Select the list of existing images
-    const imageList = document.getElementById("image-list");
+		fetch("http://localhost:5678/api/works")
+		.then(response => response.json())
+		.then(data => {
 
-    // Clear the list of existing images
-    imageList.innerHTML = "";
+			console.log(data);
 
-    // For each image in the retrieved data
-    data.forEach(image => {
-      // Create a new <img> element with the image URL
-      const newImage = document.createElement("img");
-      newImage.src = image.url;
 
-      // Create an "edit" button for the image
-      const editButton = document.createElement("button");
-      editButton.textContent = "edit";
+			// Select the list of existing images
+			const imageList = document.getElementById("image-list");
 
-      // Create a "delete" button for the image
-      const deleteButton = document.createElement("button");
-      deleteButton.textContent = "delete";
+			// Clear the list of existing images
+			imageList.innerHTML = "";
 
-      // Create a "move" button for the image
-      const moveButton = document.createElement("button");
-      moveButton.textContent = "move";
+			// For each image in the retrieved data
+			data.forEach(item => {
+				// Create a new <img> element with the image URL
+				const newImage = document.createElement("img");
+				newImage.setAttribute('src', item.imageUrl);
+				newImage.setAttribute('crossorigin', 'anonymous');
 
-      // Add the created elements to the image list
-      const imageItem = document.createElement("li");
-      imageItem.appendChild(newImage);
-      imageItem.appendChild(editButton);
-      imageItem.appendChild(deleteButton);
-      imageItem.appendChild(moveButton);
-      imageList.appendChild(imageItem);
-    });
-  })
-  .catch(error => console.error(error));
 
+
+
+
+
+
+				// Create a "move" button for the image
+				const moveButton = document.createElement("button");
+				moveButton.textContent = "";
+				moveButton.setAttribute('class', 'move-icon fa fa-arrows');
+				moveButton.addEventListener('click', (event) => {
+					// Get element clicked
+					const clickedElement = event.target.parentNode;
+					// Do something with the clicked element, like move it to another position
+					// or launch an animation to indicate that it is selected
+					alert("Fonctionnalité non implémentée");
+				});
+
+				// Create a "delete" button for the image
+				const deleteButton = document.createElement("button");
+				deleteButton.textContent = "";
+				deleteButton.setAttribute('class', 'delete-icon fa fa-trash');
+				deleteButton.setAttribute('data-id', item.id);
+				deleteButton.addEventListener('click', function(event) {
+					// @todo2
+					let result = confirm("Want to delete?");
+					if(result) {
+						//Logic to delete the item
+
+
+
+
+						// 3.3
+					// select the work element that will be removed
+          const workElement = document.getElementById('work-1');
+          // set the token value
+          const token = 'your_token_value_here';
+          // make a DELETE request to the API endpoint with the Authorization header
+          fetch('http://localhost:5678/api/works/' + item.id, {
+            method: 'DELETE',
+            headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + token
+            }
+          })
+          .then(response => {
+            if (response.ok) {
+              // if the API call is successful, remove the work element from the DOM
+              workElement.remove();
+            } else {
+              throw new Error('Erreur lors de la suppression');
+            }
+          })
+          .catch(error => console.error(error));
+
+
+
+
+
+					}
+				});
+
+				// Create an "edit" button for the image
+				const editButton = document.createElement("button");
+				editButton.textContent = "éditer";
+				editButton.setAttribute('class', 'edit-icon');
+				editButton.addEventListener('click', function(event) {
+					alert("Fonctionnalité non implémentée");
+				});
+
+				// Add the created elements to the image list
+				const imageItem = document.createElement("li");
+				imageItem.appendChild(newImage);
+				imageItem.appendChild(editButton);
+				imageItem.appendChild(deleteButton);
+				imageItem.appendChild(moveButton);
+				imageList.appendChild(imageItem);
+			});
+		})
+		.catch(error => console.error(error));
 	});
 
 	// Handling modal closing by modal click
 	document.querySelector('.modal').addEventListener('click', function(event) {
 		if(event.target.closest(".modal-content") === null) {
-			var modal = document.getElementById("myModal");
+			let modal = document.getElementById("myModal");
 			modal.style.display = "none";
 		}
 	});
 
 	// Handling modal closing by button click
-	document.getElementById('modal-close').addEventListener('click', function(event) {
-		var modal = document.getElementById("myModal");
+	document.querySelector('.modal-close').addEventListener('click', function(event) {
+		let modal = document.getElementById("myModal");
 		modal.style.display = "none";
 	});
 
@@ -166,43 +228,9 @@ document.addEventListener("DOMContentLoaded", function(event) {
 		alert("Fonctionnalité non implémentée");
 	});
 
-	// Detecting photo click on move
-	document.querySelectorAll('.move-icon').forEach(item => {
-		item.addEventListener("click", function(event) {
-			// @todo1
-      // Select all move-icons
-  const moveIcons = document.querySelectorAll('.move-icon');
-
-  // Watch all move-icons and add click event to each
-    moveIcons.forEach((icon) => {
-      icon.addEventListener('click', (event) => {
-    // Get element clicked
-    const clickedElement = event.target.parentNode;
-
-    // Do something with the clicked element, like move it to another position
-    // or launch an animation to indicate that it is selected
-      console.log('L\'élément a été déplacé ou sélectionné.');
-    });
-  });
-			alert("Fonctionnalité non implémentée");
-		});
-	});
-
-	// Detecting photo click on delete
-	document.querySelectorAll('.delete-icon').forEach(item => {
-		item.addEventListener('click', function(event) {
-			// @todo2
-      var result = confirm("Want to delete?");
-      if (result) {
-          //Logic to delete the item
-      }
-			console.log('Supprimer');
-		});
-	});
-
 	// Uploading photo
-	document.querySelector('.modal #photo').addEventListener("change", function(event) {
-		var myDiv = document.getElementsByClassName('uploader')[0];
+	document.querySelector('#modal-add-work-photo').addEventListener("change", function(event) {
+		let myDiv = document.getElementsByClassName('uploader')[0];
 		let image = document.createElement("img");
 		image.setAttribute('id','output');
 		image.setAttribute('style','width:35%;height:100%;');
@@ -220,139 +248,96 @@ document.addEventListener("DOMContentLoaded", function(event) {
 	document.querySelector('#modal-form').addEventListener("submit", function(event) {
 		event.preventDefault();
 		// @todo3
-    // Get values from form
-  const form = document.getElementById('modal-form');
-  const nameInput = document.getElementById('name-input');
-  const emailInput = document.getElementById('email-input');
+    // Get element from list
+const selectElement = document.querySelector('#modal-add-work-category');
 
-  // Add submit on form
-  form.addEventListener('submit', function(event) {
-  // Prevent form to send request
-    event.preventDefault();
+// Send GET request to API to get categories
+fetch('http://localhost:5678/api/works')
+  .then(response => response.json())
+  .then(data => {
+    // Create single board categories
+    const categories = [...new Set(data.map(work => work.category))];
 
-  // Get form values
-  const name = nameInput.value;
-  const email = emailInput.value;
-
-  // API Fetch
-  fetch("http://localhost:5678/api/works", {
-    method: 'POST',
-    body: JSON.stringify({ name: name, email: email }),
-    headers: {
-      'Content-Type': 'application/json'
-      }
-    })
-    .then(response => {
-    // Server response
-    console.log('Formulaire envoyé avec succès !');
-    })
-    .catch(error => {
-    // Request errors
-    console.error('Erreur lors de l\'envoi du formulaire : ', error);
+    // Add each category as a drop-down option
+    categories.forEach(category => {
+      const optionElement = document.createElement('option');
+      optionElement.value = category;
+      optionElement.text = category;
+      selectElement.appendChild(optionElement);
     });
-  });
+  })
+  .catch(error => console.error(error));
 
-		console.log('@todo : récupérer les champs du form et faire un fetch post qq chose');
-	  });
-  });
 
-  // @todo 4
-  // Select the close button of the pop-up
-const closeButton = document.querySelector('.close-button');
 
-// Add an event listener for the click on the close button
-closeButton.addEventListener('click', function() {
-  // Select the pop-up
-  const popup = document.querySelector('.popup');
+		// Get values from form
+		let newWorkTitle = document.getElementById('modal-add-work-title').value;
+		let newWorkCategory = document.getElementById('modal-add-work-category').value;
+		let newWorkImage = 'à chercher';
 
-  // Hide the pop-up by modifying its CSS class
-  popup.classList.add('hidden');
+		// create a new work object
+		const newWork = {
+			title:newWorkTitle,
+			category: newWorkCategory,
+			image: newWorkImage
+		};
+		console.log(newWork);
+
+		// make a POST request to the API endpoint
+		fetch('http://localhost:5678/api/works', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(newWork)
+		})
+		.then(response => {
+			if (response.ok) {
+				// if the API call is successful, parse the response as JSON
+				return response.json();
+			} else {
+				throw new Error('Erreur');
+			}
+		})
+		.then(data => {
+			// create a new work element based on the response data
+			const newWorkElement = document.createElement('div');
+			newWorkElement.classList.add('work');
+			newWorkElement.innerHTML = `
+				<h2>${data.title}</h2>
+				<p>${data.description}</p>
+			`;
+
+			// add the new work element to the DOM
+			const workList = document.getElementById('work-list');
+			workList.appendChild(newWorkElement);
+		})
+		.catch(error => console.error(error));
+	});
+
 });
 
+// Récupérer les catégories à l'ouverture de la popup via un fetch GET /categories 
+// et les ajouter au menu déroulant de la deuxième popup
 
-// 3.3
-  // select the work element that will be removed
-const workElement = document.getElementById('work-1');
+// Get the second popup
+const popup2 = document.getElementById('popup2');
 
-// make a DELETE request to the API endpoint
-fetch('http://localhost:5678/api/works', {
-  method: 'DELETE',
-  headers: {
-    'Content-Type': 'application/json'
-  },
-  body: JSON.stringify({
-    id: '1' // pass the ID of the work to be deleted
+// Get the dropdown menu in the second popup
+const select = popup2.querySelector('select');
+
+// Perform the GET request to fetch the categories
+fetch('http://localhost:5678/api/works')
+  .then(response => response.json())
+  .then(categories => {
+    // Add each category to the dropdown menu
+    categories.forEach(category => {
+      const option = document.createElement('option');
+      option.value = category.id;
+      option.textContent = category.name;
+      select.appendChild(option);
+    });
   })
-})
-.then(response => {
-  if (response.ok) {
-    // if the API call is successful, remove the work element from the DOM
-    workElement.remove();
-  } else {
-    throw new Error('Erreur lors de la suppression');
-  }
-})
-.catch(error => console.error(error));
-
-
-
-
-
-//const workId = "123"; // ID du work à supprimer
-//const authToken = "your-auth-token";
-
-//fetch("http://localhost:5678/api/works", {
-//  method: 'DELETE',
-//  headers: {
-//    'Authorization': `Bearer ${authToken}`,
-//    'Content-Type': 'application/json'
-//  }
-//})
-//.then(response => {
-//  if (!response.ok) {
-//    throw new Error(`Error in work deletion ${workId}`);
-//  }
-//  console.log(`Le work ${workId} a été supprimé avec succès`);
-// Delete existing HTML content corresponding to the work
-//  const workElement = document.getElementById(`work-${workId}`);
-//  workElement.parentNode.removeChild(workElement);
-//})
-//.catch(error => console.error(error));
-
-// 3.4
-// create a new work object
-const newWork = {
-  title: 'New Work',
-  description: 'This is a new work'
-};
-
-// make a POST request to the API endpoint
-fetch('http://localhost:5678/api/works', {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json'
-  },
-  body: JSON.stringify(newWork)
-})
-.then(response => {
-  if (response.ok) {
-    // if the API call is successful, parse the response as JSON
-    return response.json();
-  } else {
-    throw new Error('Error adding work');
-  }
-})
-.then(data => {
-  // create a new work element based on the response data
-  const newWorkElement = document.createElement('div');
-  newWorkElement.classList.add('work');
-  newWorkElement.innerHTML = `
-    <h2>${data.title}</h2>
-    <p>${data.description}</p>
-  `;
-  
-  // add the new work element to the DOM
-  const workList = document.getElementById('work-list');
-  workList.appendChild(newWorkElement);
-})
-.catch(error => console.error(error));
+  .catch(error => {
+    console.error('Une erreur est survenue lors du fetch des categories', error);
+  });
